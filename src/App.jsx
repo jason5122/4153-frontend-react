e9bfd246808b6c5b1b80c8cdec9ea41acc3b5ded
Jason Han <jason.han@columbia.edu>
2024-11-12 16:36:59 -0500

    Add example components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Component, useState } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function Greeting({ name }) {
+  return <h1>Hello, {name}!</h1>;
 }
 
-export default App
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(prevCount => prevCount + 1);
+  const decrement = () => setCount(prevCount => prevCount - 1);
+  const reset = () => setCount(0);
+
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+function UserList() {
+  const data = [
+    { username: 'person1', email: 'person1@email.com' },
+    { username: 'person2', email: 'person2@email.com' },
+  ];
+
+  return (
+    <ul>
+      {data.map((u) => <li key={u.username}>{u.email}</li>)}
+    </ul>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Greeting name='Person 1' />
+      <Counter />
+      <UserList />
+    </>
+  );
+}
+
+export default App;
